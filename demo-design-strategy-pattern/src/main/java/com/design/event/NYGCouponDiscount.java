695969122a27c0a2a7c35baf46571b099cb2fa42
junlogz <junlogz@163.com>
2023-06-13 13:01:19 +0800

    Initial commit

---
+++ demo-design-strategy-pattern/src/main/java/com/design/event/NYGCouponDiscount.java
@@ -0,0 +1,17 @@
+package com.design.event;
+
+import com.design.ICouponDiscount;
+
+import java.math.BigDecimal;
+
+public class NYGCouponDiscount implements ICouponDiscount<Double> {
+
+    /**
+     * n元购购买
+     * 1. 无论原价多少钱都固定金额购买
+     */
+    public BigDecimal discountAmount(Double couponInfo, BigDecimal skuPrice) {
+        return new BigDecimal(couponInfo);
+    }
+
+}
